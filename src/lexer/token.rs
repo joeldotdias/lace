45c05ce16472d3ff4ad72e3f34528530dc61e649
joeldotdias <joeldias0204@gmail.com>
2024-05-01 01:41:20 +0530

    Batman

---
+++ src/lexer/token.rs
@@ -0,0 +1,81 @@
+use std::fmt::Display;
+
+#[derive(Debug, PartialEq)]
+pub enum Token {
+    Ident(String),
+    Int(String),
+
+    // Operators
+    Assign, // =
+    Bang, // !
+    Plus, // +
+    Minus, // -
+    ForwardSlash, // /
+    Asterisk, // *
+    Equal, // ==
+    NotEqual, // !=
+    LessThan, // <
+    GreaterThan, // >
+    LessThanEqual, // <=
+    GreaterThanEqual, // >=
+
+    // Delimiters
+    Comma, // ,
+    Semicolon, // ;
+    LParen, // (
+    RParen, // )
+    LCurly, // {
+    RCurly, // }
+    LBracket, // [
+    RBracket, // ]
+
+    // Keywords
+    Let,
+    Function,
+    Return,
+    If,
+    Else,
+    True,
+    False,
+
+    Eof,
+    Illegal,
+}
+
+impl Display for Token {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Token::Ident(x) => write!(f, "Ident({})", x),
+            Token::Int(x) => write!(f, "Int({})", x),
+            Token::Assign => write!(f, "Assign"),
+            Token::Bang => write!(f, "Bang"),
+            Token::Minus => write!(f, "Minus"),
+            Token::Plus => write!(f, "Plus"),
+            Token::ForwardSlash => write!(f, "ForwardSlash"),
+            Token::Asterisk => write!(f, "Asterisk"),
+            Token::Equal => write!(f, "Equal"),
+            Token::NotEqual => write!(f, "NotEqual"),
+            Token::LessThan => write!(f, "LessThan"),
+            Token::GreaterThan => write!(f, "GreaterThan"),
+            Token::LessThanEqual => write!(f, "LessThanEqual"),
+            Token::GreaterThanEqual => write!(f, "GreaterThanEqual"),
+            Token::Comma => write!(f, "Comma"),
+            Token::Semicolon => write!(f, "Semicolon"),
+            Token::LParen => write!(f, "Lparen"),
+            Token::RParen => write!(f, "Rparen"),
+            Token::LCurly => write!(f, "LSquirly"),
+            Token::RCurly => write!(f, "RSquirly"),
+            Token::LBracket => write!(f, "LBracket"),
+            Token::RBracket => write!(f, "RBracket"),
+            Token::Let => write!(f, "Let"),
+            Token::Function => write!(f, "Function"),
+            Token::Return => write!(f, "Return"),
+            Token::If => write!(f, "If"),
+            Token::Else => write!(f, "Else"),
+            Token::True => write!(f, "True"),
+            Token::False => write!(f, "False"),
+            Token::Eof => write!(f, "Eof"),
+            Token::Illegal => write!(f, "Illegal"),
+        }
+    }
+}
